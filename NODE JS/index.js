9dd591f215749e172ea2e0d87d10c902ef0117a6
juanpablo alvarez <juanpablo2001alvarez@gmail.com>
2023-04-10 12:11:40 -0300

    NODE 2

---
+++ NODE JS/index.js
@@ -0,0 +1,11 @@
+var hoy= new Date();
+
+console.log('hoy es ' + hoy)
+
+var i 
+
+ for(i=0; i<10; i++){
+     console.log(i)
+}
+
+// usar comando cd para ir saltando de carpeta en carpeta, para llamar a mi archivo js es: node index.js en este caso
---
+++ NODE2/app.js
@@ -0,0 +1,58 @@
+var createError = require('http-errors');
+var express = require('express');
+var path = require('path');
+var cookieParser = require('cookie-parser');
+var logger = require('morgan');
+
+var indexRouter = require('./routes/index');
+var serviciosRouter = require('./routes/servicios'); //generar el archivo .js en routes
+var contactoRouter = require('./routes/contacto');
+
+
+var app = express();
+
+// view engine setup
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'hbs');
+
+app.use(logger('dev'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'public')));
+
+// luego de declarar mi var uso app use para linkearlo
+app.use('/', indexRouter);
+app.use('/servicios', serviciosRouter);
+app.use('/contacto', contactoRouter);
+
+
+app.get('/prueba', function(req,res){
+  res.render('hola soy la pagina de prueba de juan')  // TODA ESTA FUNCION QUIERE DECIR QUE CUANDO VAYA A LA PAGINA: http://localhost:3000/prueba VA APARECER EL SIGUIENTE TEXTO
+})
+
+app.get('/nosotros', function(req,res){
+  res.render('nosotros') // es una vista se arma en view 
+})
+
+app.get('/prueba2', function(req,res){
+  res.render('hola soy la pagina de prueba de juan 2')  
+})
+
+// catch 404 and forward to error handler
+app.use(function(req, res, next) {
+  next(createError(404));
+});
+
+// error handler
+app.use(function(err, req, res, next) {
+  // set locals, only providing error in development
+  res.locals.message = err.message;
+  res.locals.error = req.app.get('env') === 'development' ? err : {};
+
+  // render the error page
+  res.status(err.status || 500);
+  res.render('error');
+});
+
+module.exports = app;
