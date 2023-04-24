bc42c2d1661b933f69c1763c8cd4539cb43fa5e0
juanpablo alvarez <juanpablo2001alvarez@gmail.com>
2023-04-24 14:10:36 -0300

    peticiones_respuestas_templates

--- Peticiones_respuestas_templates/app.js
+++ Peticiones_respuestas_templates/app.js
@@ -3,8 +3,10 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
+var session = require('express-session'); // una vez instalada la dependencia npm i express-session poner esta linea y la linea 9
 
-require('dotenv').config(); // una vez que creamos el .inv y le pasamos los datos que esta en el archivo es necesario generar esta linea 
+
+require('dotenv').config(); // una vez que creamos el .inv y le pasamos los datos que esta en el archivo es necesario generar esta linea y de la linea26 a la 30
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
@@ -21,8 +23,77 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/', indexRouter);
-app.use('/users', usersRouter);
+app.use(session({
+  secret: 'lavalle1914', //clave de acceso
+  resave: false,
+  saveUninitialized: true
+}));
+
+// app.use('/', indexRouter);
+// app.use('/users', usersRouter);
+
+//
+app.get('/', function(req, res){
+  var conocido = Boolean(req.session.nombre);
+
+  res.render('index', {
+    title: 'sesiones en express.js',
+    conocido: conocido, // almacena el dato nombre que le pase ingrese
+    nombre: req.session.nombre
+
+  });
+});
+
+//captura los datos 
+app.post('/ingresar', function(req, res){
+  if(req.body.nombre){
+    req.session.nombre = req.body.nombre 
+  }
+res.redirect('/');
+});
+
+// para destruir la sesion y desloguear
+
+app.get('/salir', function(req, res){
+  req.session.destroy();
+  res.redirect('/');
+
+});
+
+//MIDLEWARE
+
+app.use(function(req,res,next){
+
+  if(!req.session.vistas){
+    req.session.vistas = {};
+  }
+
+    if(!req.session.vistas[req.originalUrl]){
+      req.session.vistas[req.originalUrl] = 1;
+
+    } else{
+      req.session.vistas[req.originalUrl]++;
+    }
+    console.log(req.session.vistas);
+
+    next();
+
+});
+
+app.get('/nosotros', function(req,res){
+  res.render('pagina',{
+    nombre: 'nosotros',
+    vistas: req.session.vistas[req.originalUrl]
+  });
+});
+
+
+app.get('/contacto', function(req,res){
+  res.render('pagina',{
+    nombre: 'contacto',
+    vistas: req.session.vistas[req.originalUrl]
+  });
+});
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
